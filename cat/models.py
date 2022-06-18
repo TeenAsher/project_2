e001a6e5b45f66a819ea1e265635b208d1e2f72f
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-06-18 16:40:49 +0300

    Created a model Entry()

--- cat/models.py
+++ cat/models.py
@@ -15,3 +15,26 @@ class Topic(models.Model):
         """A string representation of the model"""
 
         return self.text
+
+
+class Entry(models.Model):
+
+    """Something specific about the topic"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+
+        """A string representation of the model"""
+
+        len_text = len(self.text[:])
+        if len_text <= 50:
+            return f'{self.text[:]}'
+        else:
+            return f'{self.text[:50]}...'
+
