063f98512c297186eb1ebf27eaedd6d91e6a5d21
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-06-20 18:18:26 +0300

    Added the topic.html

--- cat/views.py
+++ cat/views.py
@@ -18,3 +18,14 @@ def topics(request):
     context = {'topics': topics}
 
     return render(request, 'cat/topics.html', context)
+
+
+def topic(request, topic_id):
+
+    """Shows a single topic and its entries"""
+
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'cat/topic.html', context)
